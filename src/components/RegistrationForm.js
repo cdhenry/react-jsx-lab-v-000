246202b9af92bcedc084b763c8419d4ac756ad22
Chris Henry <cdhenry@gmail.com>
2018-05-11 00:24:24 +0000

    automatically backed up by learn

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,12 @@
+class RegistrationForm extends React.Component {
+  render() {
+    return (
+      <div className="registrationForm">
+        <img src="http://twitter.com/some-avatar.png" className="tweet__avatar" />
+        <div className="tweet__body">
+            <p>We are writing this tweet in JSX. Holy moly!</p>  
+        </div>
+      </div>
+    );
+  }
+}
