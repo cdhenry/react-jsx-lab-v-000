8b435ea8428940b305c36f203d45928642f20638
Chris Henry <cdhenry@gmail.com>
2018-05-11 00:26:59 +0000

    automatically backed up by learn

---
+++ src/components/Webpage.js
@@ -0,0 +1,9 @@
+class Webpage extends React.Component {
+  render() {
+    return (
+      <div className="registrationForm">
+        <p>I am a filler text. I can be used to fill your screen. Amazing!</p>
+      </div>
+    );
+  }
+}
